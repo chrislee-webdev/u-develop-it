cafc66f3b9e4707ac229cdac15874d0379c7c634
Christopher <leechrs1@gmail.com>
2022-05-22 22:06:50 -0500

    commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
+const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
-const express = require('express');
-
-const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,12 +9,11 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('api', apiRoutes);
+// Use apiRoutes
+app.use('/api', apiRoutes);
 
-
-
-// Not Found response for unmatched routes
-app.use((req, res) => {
+// Default response for any other request (Not Found)
+app.use((req, res) => { 
   res.status(404).end();
 });
 
